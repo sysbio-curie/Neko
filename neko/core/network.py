4c4057c4ca4a25babf914808e604d63437cabda0
deeenes <turei.denes@gmail.com>
2024-04-19 15:26:55 +0200

    `Network.filter_unsigned_paths` → `_methods.misc`

--- neko/core/network.py
+++ neko/core/network.py
@@ -642,33 +642,6 @@ class Network:
         # Check if all nodes are visited
         return set(self.nodes['Uniprot']) == visited
 
-    def filter_unsigned_paths(self, paths: list[tuple], consensus: bool) -> list[tuple]:
-        """
-        This function filters out unsigned paths from the provided list of paths. An unsigned path is a path where at least
-        one interaction does not have a defined sign (stimulation or inhibition). The function checks each interaction in each
-        path, and if the interaction is unsigned, the path is not included in the output list.
-
-        Parameters:
-        - paths: A list of paths, where each path is a sequence of nodes.
-        - consensus: A boolean indicating whether to check for consensus among references when determining the sign of an interaction.
-
-        Returns:
-        - A list of paths where all interactions in each path are signed.
-        """
-        interactions = self.resources
-        filtered_paths = []
-        for path in paths:
-            is_full_signed = True
-            for i in range(0, len(path)):
-                if i == len(path) - 1:
-                    break
-                interaction = interactions.loc[(interactions["source"] == path[i]) &
-                                               (interactions["target"] == path[i + 1])]
-                if not interaction.empty and check_sign(interaction, consensus) == "undefined":
-                    is_full_signed = False
-            if is_full_signed:
-                filtered_paths.append(path)
-        return filtered_paths
 
     def check_node_existence(self, node: str) -> bool:
         """
@@ -696,4 +669,3 @@ class Network:
         self.edges["target"] = self.edges["target"].apply(convert_identifier)
 
         return
-

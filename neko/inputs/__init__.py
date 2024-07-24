5c44d58ecc598c3554db336cf593917db8cb2746
deeenes <turei.denes@gmail.com>
2024-07-24 20:00:30 +0200

    `inputs`: updated exports

--- neko/inputs/__init__.py
+++ neko/inputs/__init__.py
@@ -1,9 +1,5 @@
-from ._db.omnipath import omnipath_universe
-from ._universe import network_universe, Universe
-
 """
 Access to generic networks from databases, files and standard formats.
 """
 
-from ._universe import Universe
-from ._db import (signor, psp, omnipath)
+from ._universe import Universe, signor, phosphosite, omnipath

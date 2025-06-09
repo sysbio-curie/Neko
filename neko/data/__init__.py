3c704bf71cbdc22317117646805cfc295db71eb6
marcorusc <m.ruscone94@gmail.com>
2025-06-09 15:43:29 +0200

    refactor: remove networkcommons dependency and use direct data access for phosphosite and huri

--- neko/data/__init__.py
+++ neko/data/__init__.py
@@ -1,35 +1,38 @@
 import urllib.parse
-
+import requests
 import pandas as pd
-from networkcommons.data.omics import _common as _ncdata
-
-
-def _retrieve(path: str, ftype: str = 'tsv') -> pd.DataFrame:
-
-    url = urllib.parse.urljoin(_ncdata._baseurl(), path)
-
-    return _ncdata._open(url, ftype = ftype, df = True)
 
 
 def phosphosite_kinase_substrate():
-
-    return _retrieve('phosphosite/kinase-substrate.tsv')
+    url = 'https://www.phosphosite.org/downloads/Kinase_Substrate_Dataset.gz'
+    try:
+        df = pd.read_csv(url, compression='gzip', sep='\t', low_memory=False)
+    except Exception:
+        # fallback for direct download if pandas fails
+        r = requests.get(url)
+        r.raise_for_status()
+        from io import BytesIO
+        df = pd.read_csv(BytesIO(r.content), compression='gzip', sep='\t', low_memory=False)
+    return df
 
 
 def phosphosite_regulatory_sites():
+    url = 'https://www.phosphosite.org/downloads/Regulatory_sites.gz'
+    try:
+        df = pd.read_csv(url, compression='gzip', sep='\t', low_memory=False)
+    except Exception:
+        r = requests.get(url)
+        r.raise_for_status()
+        from io import BytesIO
+        df = pd.read_csv(BytesIO(r.content), compression='gzip', sep='\t', low_memory=False)
+    return df
 
-    return _retrieve('phosphosite/regulatory-sites.tsv')
 
-
-def huri(dataset: str = 'HI-union', translated = True) -> pd.DataFrame:
-
+def huri(dataset: str = 'HI-union', translated=True) -> pd.DataFrame:
     url = (
         'https://github.com/sysbio-curie/Medulloblastoma_project/'
         'raw/main/Huri_analysis/data/%s%s.csv'
     )
-
     url = url % (dataset, '_translated' if translated else '')
-
-    df = _ncdata._open(url, df = True)
-
+    df = pd.read_csv(url)
     return df
